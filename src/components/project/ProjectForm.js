296fd07cd64c866fd7ccdf11b43725de742702c4
Thiago <thiagooffice2@gmail.com>
2022-02-19 22:12:54 -0300

    Simulador de API, JSON Server

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,28 @@
+import {useState, useEffect} from 'react'
 import Input from '../form/input'
 import Select from '../form/Select'
 import styles from './ProjectForm.module.css'
 import SubmitButton from '../form/SubmitButton'
 
 export default function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories",  {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(( resp) => resp.json())
+    .then((data)=>{
+        setCategories(data)
+    })
+    .catch(err => console.log(err))
+    
+    },[])
+
     return(
         <form className={styles.form}>
 
@@ -18,6 +37,7 @@ export default function ProjectForm({btnText}){
            <Select 
            name="category_id" 
            text="Selecione a categoria"
+           options={categories}
            />
             <SubmitButton text={btnText} />
         </form>
